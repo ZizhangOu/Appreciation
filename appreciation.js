ecff170564258028ceb9d3c1b7f8a2ee57b27efb
Zizhang Ou Kevin Zen <ouzizhangkevin@gmail.com>
2022-12-13 18:18:26 +1100

    The first version of Appreciation App, finished basic Html, CSS, Javascript

---
+++ appreciation.js
@@ -0,0 +1,30 @@
+const content = document.getElementById('content')
+const addedBtn = document.getElementById('addedBtn')
+
+const listContent = []
+
+addedBtn.addEventListener('click',function(){
+    console.log('content',content.value)
+
+    listContent.unshift({
+        content:content.value
+    })
+
+    let htmlStr = ''
+
+    listContent.forEach(function(item){
+        htmlStr = htmlStr + 
+        '
+        <div class="item">
+          <div>
+            <p>content：${item.content}</p>
+          </div>
+        </div>
+        '
+    
+
+    })
+
+    list.innerHTML = htmlStr
+
+})
